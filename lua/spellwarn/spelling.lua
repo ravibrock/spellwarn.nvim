cbc725f9bcb046458389a65ace35617ad98c9f62
ravibrock <66334356+ravibrock@users.noreply.github.com>
2024-06-02 16:16:23 -0500

    feat: add syntax to disable on lines/files

--- lua/spellwarn/spelling.lua
+++ lua/spellwarn/spelling.lua
@@ -10,6 +10,7 @@ function M.get_spelling_errors(bufnr)
     -- Save current window view and create table to store errors
     local window = vim.fn.winsaveview()
     local errors = {}
+    if string.find(vim.fn.getline(1), "spellwarn:disable", 1, true) ~= nil then return errors end
 
     -- Get location of first spelling error to start while loop
     vim.fn.setpos(".", { bufnr, 1, 1, 0 })
@@ -17,7 +18,16 @@ function M.get_spelling_errors(bufnr)
     vim.cmd("silent normal! ]s")
     local location = vim.fn.getpos(".")
 
+    local function check_spellwarn_comment() -- Check for spellwarn:disable* comments
+        local current_line_number = vim.fn.line(".")
+        local above = (current_line_number > 1 and vim.fn.getline(current_line_number - 1)) or ""
+        local above_val = string.find(above, "spellwarn:disable-next-line", 1, true) ~= nil
+        local cur = vim.fn.getline(current_line_number)
+        local cur_val = string.find(cur, "spellwarn:disable-line", 1, true) ~= nil
+        return above_val or cur_val
+    end
     local function adjust_table() -- Add error to table
+        if check_spellwarn_comment() then return end
         local word = vim.fn.expand("<cword>")
         table.insert(errors, {
             col  = location[3],
