f142f500a2c474070d6306eb0cf7943882cfbb8e
Nadelio <138712393+Nadelio@users.noreply.github.com>
2025-08-29 14:52:54 -0500

    Fixed issue #14

--- lua/spellwarn/diagnostics.lua
+++ lua/spellwarn/diagnostics.lua
@@ -24,21 +24,23 @@ function M.update_diagnostics(opts, bufnr)
     for _, error in pairs(require("spellwarn.spelling").get_spelling_errors_main(opts, bufnr) or {}) do
         local suggestions = vim.fn.spellsuggest(error.word)
 				
-				local msg = opts.prefix .. error.word
+		local msg = opts.prefix .. error.word
 				
-				if opts.suggest then
-					  local addition = "\nSuggestions: "
-					  for i = 1, opts.num_suggest do
-						    if i == opts.num_suggest then
-							    	addition = addition .. i .. '. ' .. suggestions[i]
-						    else
-						    		addition = addition .. i .. '. ' .. suggestions[i] .. '\n'
-						    end
-					  end
-					  msg = msg .. addition
+		if opts.suggest then
+			local addition = "\nSuggestions: "
+			for i = 1, opts.num_suggest do
+				if suggestions[i] then
+					if i == opts.num_suggest then
+						addition = addition .. i .. '. ' .. suggestions[i]
+					else
+						addition = addition .. i .. '. ' .. suggestions[i] .. '\n'
+					end
 				end
+			end
+			msg = msg .. addition
+		end
 
-				if error.word ~= "" and error.word ~= "spellwarn" then
+		if error.word ~= "" and error.word ~= "spellwarn" then
             if opts.severity[error.type] then
                 diags[#diags + 1] = {
                     col      = error.col - 1, -- 0-indexed
@@ -108,4 +110,5 @@ function M.setup(opts)
 end
 
 return M
+
 
